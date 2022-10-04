dba3a41a4dd5e8247cd39b8d24c580ceeaa90e10
Stephanie Cruz <exscruzme@gmail.com>
2022-10-04 16:31:38 -0400

    refactored styling syntax

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,29 @@
 <div class="banner">
   <div class="banner-text">
     <img src='csma-technology-title.png' alt='CSMA Technology' />
+
     <h1>
       PASSIONATE ABOUT TECHNOLOGY,
       <br />
       INSPIRED TO BUILD.
     </h1>
+
     <h2 class="subtitle">
       We’re here to find effective solutions to interesting problems.
     </h2>
+
+  </div>
+</div>
+
+<div class="blurb">
+  <p class="blurb-text">Based in South Florida, CSMA Technology is an independent software and consulting company, 
+    excited to work clients to provide quality software, with a focus on growing the local community.
+    We build websites, cross-platform applications, and custom software solutions.
+  </p>
+
+  <div>
+    Button 1
+    Button 2
   </div>
 </div>
 
@@ -16,7 +31,7 @@
   .banner {
     height: 400px;
     display: flex;
-    flex-flow: row wrap;
+    align-items: center;
     justify-content: center;
     background-image: url('CSMA_Graphic_Waves_Top_Right.png');
     background-size: cover;
@@ -26,10 +41,17 @@
   .banner-text {
     text-align: left;
     color: white;
+    width: 975px;
+  }
+
+  .blurb {
     display: flex;
     flex-flow: column wrap;
-    align-items: flex-start;
-    justify-content: center;
-    width: 975px;
+    align-items: center;
+  }
+  
+  .blurb-text {
+    width: 1000px;
+    text-align: left;
   }
 </style>
