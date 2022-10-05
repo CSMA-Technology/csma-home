2a1f7d8cf0746784725d4381b60934329e3f8409
Stephanie Cruz <exscruzme@gmail.com>
2022-10-05 17:51:36 -0400

    added contact us form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,11 +1,63 @@
 <h1>Contact Us</h1>
 <p>Tell us what you need, and we'll work together to bring it to life.</p>
-<div>
-  <div>
-    <input type="text" id="name" />
+<div class="contact-form">
+  <div class="inputs">
+    <div class="names">
+      <div class="text-field">
+        <input type="text" id="first-name" />
+        <label for="first-name">First Name</label>
+      </div>
+      <div class="text-field">
+        <input type="text" id="last-name" />
+        <label for="last-name">Last Name</label>
+      </div>
+    </div>
+    <div class="text-field">
+      <input type="text" id="email" />
+      <label for="email">Email</label>
+    </div>
+    <div class="text-field">
+      <textarea id="msg" rows=10 cols=40 />
+      <label for="msg">Message</label>
+    </div>
   </div>
+  <button>Submit</button>
 </div>
 
 <style>
+  input[type=text] {
+    height: 25px;
+    border: 1px solid #000000;
+    border-radius: 5px;
+    min-width: 300px;
+  }
 
+  textarea {
+    border: 1px solid #000000;
+    border-radius: 5px;
+  }
+
+  .inputs {
+    width: 700px;
+    display: flex;
+    flex-flow: column wrap;
+  }
+
+  .contact-form {
+    display: flex;
+    flex-flow: column wrap;
+    align-items: center;
+  }
+
+  .names {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+  }
+
+  .text-field {
+    display: flex;
+    flex-flow: column wrap;
+    text-align: left;
+  }
 </style>
