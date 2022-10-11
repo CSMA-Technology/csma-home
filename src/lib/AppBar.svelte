6e52509432227539c23e97481830ef982d6da5e8
admoya <moyaa1022@gmail.com>
2022-10-11 15:16:38 -0400

    feat(responsiveness): made site responsive (#2)
    * feat(responsiveness): made home page responsive
    
    * feat(menu): added fade transition
    
    * feat(services): made services page responsive
    
    * feat(pictures): added adrian picture
    
    * fix(font-size): changed font size default to percentage
    
    * changed font of banner subtitle on services page, made the service cards 75% of width"
    
    * changed order of navlinks
    
    * reordered nav links
    
    * also applied column reverse to hamburger menu
    
    * fix(banners): adjusted banner styles
    
    * fix(avatars): adjusted avatar corners
    
    Co-authored-by: Stephanie Cruz <exscruzme@gmail.com>

--- src/lib/AppBar.svelte
+++ src/lib/AppBar.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
   import csmaLogo from '$lib/assets/turtle-logo.png';
+
+  var isMenuopen = false;
 </script>
 
 <nav class="app-bar">
@@ -7,20 +9,31 @@
       <img id="csmaLogo" src={csmaLogo} alt="The CSMA logo" />
       <p>CSMA</p>
   </a>
-  <ul class="nav-links">
+  <ul on:click={() => { isMenuopen = false }} class={`nav-links ${isMenuopen ? '' : 'hide-menu'}`}>
+    <li>
+      <a href='/projects'>Projects</a>
+    </li>
+    <li>
+      <a href='/about'>About</a>
+    </li>
     <li>
       <a href='/services'>Services</a>
     </li>
     <li>
       <a href='/contact'>Contact</a>
     </li>
-    <li>
-      <a href='/about'>About</a>
-    </li>
-    <!-- <li>
-      <a href='/projects'>Projects</a>
-    </li> -->
   </ul>
+  <button on:click={() => { isMenuopen = !isMenuopen }} class="hamburger-button">
+    {#if isMenuopen}
+      <span class="material-symbols-outlined">
+        close
+      </span>
+    {:else}
+      <span class="material-symbols-outlined">
+        menu
+      </span>
+    {/if}
+  </button>
 </nav>
 
 <style>
@@ -30,6 +43,7 @@
     background-color: #00838F;
     display: flex;
     flex-direction: row;
+    background-size: 100%;
   }
 
   .branding {
@@ -39,6 +53,7 @@
     margin-left: .5rem;
     text-align: center;
     gap: .75rem;
+    max-width: 156px;
   }
 
   .branding p {
@@ -57,8 +72,10 @@
   }
 
   .nav-links {
+    flex-grow: 1;
     display: flex;
-    flex-direction: row;
+    flex-direction: row-reverse;
+    margin-right: 2rem;
     font-family: Overpass;
     font-size: 20px;
     font-weight: 700;
@@ -71,6 +88,43 @@
 
   .nav-links a {
     color: white;
+  }
+
+  .hamburger-button {
+    display: none;
+  }
+
+  @media (max-width: 640px) {
+    .hide-menu {
+      opacity: 0;
+      visibility: hidden;
+    }
+
+    .hamburger-button {
+      display: block;
+      width: auto;
+      background: none;
+      color: white;
+      margin-left: auto;
+      margin-right: 1rem;
+    }
+    .nav-links {
+      flex-direction: column-reverse;
+      z-index: 100;
+      background-color: #004046;
+      margin-top: 66px;
+      width: 100%;
+      position: absolute;
+      padding-left: 0;
+      text-align: right;
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+      transition: opacity 600ms, visibility 600ms;
+    }
+
+    .nav-links a {
+      padding-right: 2rem;
+    }
   }
 
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,23 +5,22 @@
 
 <div class="banner" style="background-image: url({homeBanner}">
   <div class="banner-text">
-    <img src={csmaTitle} alt='CSMA Technology' />
+    <img class="banner-image" src={csmaTitle} alt='CSMA Technology' />
 
-    <h1>
+    <h1 class="banner-title">
       PASSIONATE ABOUT TECHNOLOGY,
       <br />
       INSPIRED TO BUILD.
     </h1>
 
-    <h2 class="subtitle">
+    <h2 class="banner-subtitle">
       We’re here to find effective solutions to interesting problems.
     </h2>
 
   </div>
 </div>
-
 <div class="blurb">
-  <p class="blurb-text">Based in South Florida, CSMA Technology is an independent software and consulting company, 
+  <p class="blurb-text">Based in South Florida, CSMA Technology is an independent software and consulting company,
     excited to work clients to provide quality software, with a focus on growing the local community.
     We build websites, cross-platform applications, and custom software solutions. Our founders, Adrian and Stephanie,
     started this company to bring innovative projects to life. If this sounds exciting, we'd love to work with you!
@@ -40,28 +39,37 @@
 </div>
 
 <style>
-  .banner-text {
-    text-align: left;
-    color: white;
-    width: 975px;
+  .banner-image {
+    margin-top: 2rem;
+    max-width: 60%;
   }
 
   .blurb {
     display: flex;
     flex-flow: column wrap;
     align-items: center;
+    max-width: 60rem;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 1rem;
+    padding-right: 1rem;
   }
-  
+
   .blurb-text {
-    padding: 1em;
-    width: 1000px;
     text-align: left;
   }
 
   .blurb-buttons {
-    width: 600px;
+    width: 100%;
     display: flex;
     flex-flow: row wrap;
-    justify-content: space-between;
+    justify-content: space-around;
+    gap: 1rem;
+  }
+
+  @media (max-width: 640px) {
+    .blurb {
+      padding: 1rem;
+    }
   }
 </style>
