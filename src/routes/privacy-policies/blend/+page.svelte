22d6a6c7e3bd7048a1e00d6623abb7480fddece4
Adrian Moya <moyaa1022@gmail.com>
2023-01-11 19:37:11 +0000

    feat(privacy-policy): added Blend privacy policy

---
+++ src/routes/privacy-policies/blend/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+import policy from './policy';
+</script>
+
+<div class="paper">
+  <div class="policy">
+    {@html policy}
+  </div>
+</div>
+
+<style>
+  .policy {
+    max-width: 70rem;
+    margin: auto;
+  }
+</style>
