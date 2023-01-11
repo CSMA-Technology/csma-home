2c9f71c6d1dca484510a8ab8213a5ac75ca7230e
Adrian Moya <moyaa1022@gmail.com>
2023-01-11 20:04:41 +0000

    feat(text): justified policy text

--- src/routes/privacy-policies/blend/+page.svelte
+++ src/routes/privacy-policies/blend/+page.svelte
@@ -12,5 +12,10 @@ import policy from './policy';
   .policy {
     max-width: 70rem;
     margin: auto;
+    text-align: justify;
   }
+  :global(.policy h1, h2, h3, h4) {
+    text-align: center;
+  }
+
 </style>
