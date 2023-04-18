8f6e2b3a9dc882a350b5092181315c7ae2295ade
Stephanie Cruz <exscruzme@gmail.com>
2023-04-18 19:25:42 -0400

    feat(project-redesign): updated Blend content and formatting of featured projects

--- src/lib/components/FeaturedProject.svelte
+++ src/lib/components/FeaturedProject.svelte
@@ -5,60 +5,41 @@
 
 <div class="feature">
 	<div class="feature-images">
-    <img src={blendPlay} alt="Blend App Play Screen" />
-    <p class="caption">Caption about the photo</p>
+    <a href="https://blendreading.com" target="_blank" rel="noopener noreferrer"><img src={blendPlay} alt="Blend App Play Screen" /></a>
   </div>
 	<div class="text-content">
-		<a href="https://blendreading.com" target="_blank" rel="noopener noreferrer"
-			><h2>Blend</h2></a
-		>
-		<p>
-			Blend is a modern take on building literacy in young students. 
-      It is a virtual blending board made to transform phonics instruction for teachers. 
-      We built Blend in collaboration with Sage Literacy.
-		</p>
-		<h3>Highlighted work</h3>
-		<ul>
-			<li>Innovative virtual offering for physical blending boards</li>
-			<li>Stripe integration for subscriptions processing</li>
-			<li>Responsive product site beautifully highlighting Blend's features</li>
-			<li>Listmonk setup for marketing campaigns and consumer updates</li>
-		</ul>
-    <ul class="tags">
+		<a href="https://blendreading.com" target="_blank" rel="noopener noreferrer"><h2>Blend</h2></a>
+		<ul class="tags">
       <li>Stripe</li>
       <li>Godot</li>
       <li>Firebase</li>
       <li>Netlify</li>
       <li>Svelte</li>
       <li>TypeScript</li>
+			<li>Listmonk</li>
     </ul>
+		<p>
+			CSMA's founders have always had an interest in the intersection of education and technology. So when our friend and founder of 
+			<a href="https://www.sageliteracy.org/" target="_blank" rel="noopener noreferrer">Sage Literacy</a>, Summer Kiesel,
+			approached us with the idea of virtualizing a physical teaching tool that she uses every day, we were excited to build something
+			that could revolutionize the way teachers teach phonics. Blend is our collaborative take on the modern blending board.
+		</p>
+		<ul>
+			<li>Fully interactive virtual offering for physical blending boards</li>
+			<li>Stripe integration for subscriptions processing</li>
+			<li>Responsive product site highlighting Blend's features</li>
+			<li>Custom mailing list solution for marketing campaigns and consumer updates</li>
+		</ul>
 	</div>
 </div>
 
 <div class="feature">
 	<div class="feature-images">
-    <img src={khaHome} alt="KHA Website Home Page" />
-    <p class="caption">Caption about the photo</p>
+    <a href="https://kendalehoa.org" target="_blank" rel="noopener noreferrer"><img src={khaHome} alt="KHA Website Home Page" /></a>
   </div>
 	<div class="text-content">
-		<a href="https://kendalehoa.org" target="_blank" rel="noopener noreferrer"
-			><h2>KHA Website</h2></a
-		>
-		<p>
-			Our client, Kendale Homeowners Association, wanted a new, modernized website to attract new
-			members and keep engaged with their current member base. We redesigned and built their new
-			website entirely from scratch, and elevated its functionality with new features tailored to
-			our client's needs.
-		</p>
-		<h3>Highlighted work</h3>
-		<ul>
-			<li>Custom design and build of a responsive, lightweight website</li>
-			<li>PayPal and Venmo integrations for onsite payment processing of membership dues</li>
-			<li>Embedded Instagram feed to display the organization's latest posts and updates</li>
-			<li>Listmonk setup for centralized email campaign management for the KHA board</li>
-			<li>Custom logo design for KHA's branding</li>
-		</ul>
-    <ul class="tags">
+		<a href="https://kendalehoa.org" target="_blank" rel="noopener noreferrer"><h2>KHA Website</h2></a>
+		<ul class="tags">
       <li>PayPal</li>
       <li>Venmo</li>
       <li>Firebase</li>
@@ -67,62 +48,56 @@
       <li>Svelte</li>
       <li>TypeScript</li>
     </ul>
+		<p>
+			Our client, Kendale Homeowners Association, wanted to reimagine their website with a focus on attracting new
+			members and keeping engaged with their current member base. We redesigned and built their new
+			website entirely from scratch, and elevated its functionality with new features tailored to
+			our client's needs.
+		</p>
+		<ul>
+			<li>Custom design and build of a responsive, lightweight website</li>
+			<li>PayPal and Venmo integrations for onsite payment processing</li>
+			<li>Embedded Instagram feed to display KHA's latest posts and updates</li>
+			<li>Custom mailing list solution for centralized email campaign management used by the KHA board</li>
+			<li>Custom logo design for KHA's branding</li>
+		</ul>
 	</div>
 </div>
 
 <style>
 	.feature {
+		width: 80%;
 		display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-		align-items: center;
-		justify-content: space-evenly;
-		gap: 1.2rem;
+		align-items: flex-start;
+		justify-content: center;
+		gap: 2rem;
     margin: 20px;
 	}
 
 	.feature-images {
-    display: flex;
-    flex-direction: column;
+		
+		flex-basis: 36em;
 		width: 50%;
 	}
 
   .feature-images img {
-    border-radius: 10px 10px 0 0;
+    border-radius: 10px;
     width: 100%;
+		object-fit: contain;
   }
 
 	.text-content {
     width: 40%;
+		flex-basis: 35em;
 		text-align: left;
 	}
 
 	h2 {
-		margin-bottom: 0;
-		font-family: 'Lora';
-		font-size: 2.2rem;
+		margin: 0;
+		font-size: 2.5rem;
 	}
-
-	h3 {
-		margin-bottom: 0;
-		font-family: 'Lora';
-		font-size: 1.5rem;
-	}
-
-	a {
-		color: white;
-	}
-	a:hover {
-		color: #00838f;
-	}
-  .caption {
-    font-size: 1rem;
-    margin: 0;
-    background-color: white;
-    padding: 0.5rem;
-    color: black;
-    border-radius: 0 0 10px 10px;
-  }
 
   .tags {
     padding: 0;
