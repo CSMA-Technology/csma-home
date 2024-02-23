d6a70ed1723b18807565d541392d92edff724bcf
Adrian Moya <adrian@csma.tehcnology>
2024-02-23 20:34:02 +0000

    fix(app-bar): give logo a win width

--- src/lib/AppBar.svelte
+++ src/lib/AppBar.svelte
@@ -6,8 +6,8 @@
 
 <div class="fixed top-0 left-0 w-full bg-none z-10">
 	<nav class="app-bar bg-surface-500 rounded-3xl mx-auto w-fit m-1 border-tertiary-500 border-2">
-		<a class="branding ml-2 mr-2 md:mr-8" href="/">
-			<img id="csmaLogo" src={csmaLogo} alt="The CSMA logo" />
+		<a class="branding ml-2 mr-1 md:mr-8" href="/">
+			<img id="csmaLogo" class="min-w-10" src={csmaLogo} alt="The CSMA logo" />
 			<p class="font-heading-token text-2xl ml-1 max-md:hidden">CSMA</p>
 		</a>
 		<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
@@ -18,17 +18,17 @@
 			on:keypress={() => {
 				isMenuopen = false;
 			}}
-			class={`nav-links ${isMenuopen ? '' : 'hide-menu'}`}>
-			<li class="text-lg">
+			class="flex flex-row-reverse gap-3 items-center mx-2">
+			<li>
 				<a href="/projects">Projects</a>
 			</li>
-			<li class="text-lg">
+			<li>
 				<a href="/about">About</a>
 			</li>
-			<li class="text-lg">
+			<li>
 				<a href="/services">Services</a>
 			</li>
-			<li class="text-lg">
+			<li>
 				<a href="/contact">Contact</a>
 			</li>
 		</ul>
@@ -73,66 +73,7 @@
 		margin-top: -4px;
 	}
 
-	.nav-links {
-		flex-grow: 1;
-		display: flex;
-		flex-direction: row-reverse;
-		margin-right: 2rem;
-		font-family: Overpass;
-		font-size: 20px;
-		font-weight: 700;
-		line-height: 26px;
-		letter-spacing: 0em;
-		text-align: center;
-		list-style-type: none;
-		align-items: center;
-		gap: 1rem;
-	}
-
-	.nav-links a {
-		color: white;
-	}
-
-	.nav-links li {
-		height: fit-content;
-	}
-
 	.hamburger-button {
 		display: none;
 	}
-	/* 
-	@media (max-width: 640px) {
-		.hide-menu {
-			opacity: 0;
-			visibility: hidden;
-		}
-
-		.hamburger-button {
-			display: block;
-			width: auto;
-			background: none;
-			color: white;
-			margin-left: auto;
-			margin-right: 1rem;
-		}
-		.nav-links {
-			flex-direction: column-reverse;
-			z-index: 100;
-			background-color: #004046;
-			margin-top: 66px;
-			width: 100%;
-			position: absolute;
-			padding-left: 0;
-			text-align: right;
-			padding-top: 1rem;
-			padding-bottom: 1rem;
-			transition:
-				opacity 600ms,
-				visibility 600ms;
-		}
-
-		.nav-links a {
-			padding-right: 2rem;
-		}
-	} */
 </style>
